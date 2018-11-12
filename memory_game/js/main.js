f31b4bb247cd66ef7afdb116ef931079e32b30a7
Joshua King <josh@users-air.home>
2018-11-12 18:29:27 +0000

    added in arrays and if statement

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,19 @@
-var cardOne = "king"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "queen"
+var cards = ["queen", "king", "queen", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match! Good job!");
+	} else{
+		alert("Sorry, try again. ");
+	};
+}
+// else{
+// 	alert("you haven't selected two yet")
+// }
